4618e8631e16caf2a0488f50b702c8f9a5d71fa6
Marcus Dantas <marcus@dantas.co.uk>
2020-06-18 15:48:50 +0100

    Grid frontend function

--- index.js
+++ index.js
@@ -26,20 +26,20 @@ function createGrid(x) {
 // create a 16x16 grid when the page loads
 // creates a hover effect that changes the color of a square to black when the mouse passes over it, leaving a (pixel) trail through the grid
 // allows the click of a button to prompt the user to create a new grid
-// $(document).ready(function() {
-//     createGrid(16);
+$(document).ready(function() {
+    createGrid(16);
 
-//     $(".grid").mouseover(function() {
-//         $(this).css("background-color", "black");
-//         });
+    $(".grid").mouseover(function() {
+        $(this).css("background-color", "black");
+        });
 
-//     $(".newGrid").click(function() {
-//         refreshGrid();
+    $(".newGrid").click(function() {
+        refreshGrid();
 
-//         $(".grid").mouseover(function() {
-//         $(this).css("background-color", "black");
-//         });
-//     });
-// });
+        $(".grid").mouseover(function() {
+        $(this).css("background-color", "black");
+        });
+    });
+});
 
-createGrid(40)
+createGrid(40);
